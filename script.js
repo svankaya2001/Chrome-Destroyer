2c8da4639bb619a39b080bfe2afd87cb84b656f9
Shreyas Vankayalapati <svankayalapati@ucdavis.edu>
2022-04-16 11:40:03 -0700

    google to bing

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ chrome.webRequest.onBeforeRequest.addListener(
   function(details){
     return {redirectUrl: "https://www.bing.com"};
   },
-  {urls:["*://*.google.com"]},
+  {
+    urls:["*://*.google.com/*"],
+    types: ['main_frame', 'sub_frame'],
+  },
   ["blocking"]
 );
